4d1c3ef4846018bf24fa0e8d79e49313960a66d9
Syns369 <synss@protonmail.com>
2023-09-26 16:34:30 +0200

    first commit

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdarcour <jdarcour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 15:39:59 by jdarcour          #+#    #+#             */
+/*   Updated: 2023/09/26 15:47:52 by jdarcour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "libft-42/libft.h"
+
+# include <signal.h>
+# include <unistd.h>
+
+int	main(int argc, char **argv);
+
+#endif
