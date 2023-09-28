b74a15b617751d076ac6db490c37c1316e29abc5
Syns369 <synss@protonmail.com>
2023-09-28 19:08:49 +0200

    beaucoup de changement

--- srcs/client.c
+++ srcs/client.c
@@ -6,29 +6,74 @@
 /*   By: jdarcour <jdarcour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:22:24 by jdarcour          #+#    #+#             */
-/*   Updated: 2023/09/26 18:27:46 by jdarcour         ###   ########.fr       */
+/*   Updated: 2023/09/28 16:07:52 by jdarcour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	send_message(int pid, char *message)
+void	send_char(int pid, unsigned char character)
 {
-	(void)message;
-	kill(pid, SIGUSR1);
+	int				i;
+	unsigned char	octet_tmp;
+
+	octet_tmp = character;
+	i = 8;
+	while (i-- > 0)
+	{
+		octet_tmp = character >> i;
+		if (octet_tmp % 2 == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+int	send_len(int pid, int len)
+{
+	int	i;
+	int	bit;
+
+	i = 31;
+	while (i >= 0)
+	{
+		bit = (len >> i) & 1;
+		if (bit == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(100);
+		i--;
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+	int		pid;
+	char	*message;
+	int		i;
+	int		message_len;
 
 	if (argc != 3)
 	{
 		ft_printf("Usage: ./client [pid] [message]\n");
-		return (0);
+		exit(0);
 	}
 	pid = ft_atoi(argv[1]);
+	message = argv[2];
 	ft_printf("Sending message to %d\n", pid);
-	send_message(pid, argv[2]);
+	message_len = ft_strlen(message);
+	send_len(pid, message_len);
+	usleep(100);
+	i = 0;
+	while (message[i])
+	{
+		send_char(pid, (unsigned char)message[i]);
+		i++;
+	}
+	// send_char(pid, (unsigned char) '\n');
+	send_char(pid, (unsigned char) '\0');
 	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,16 +6,69 @@
 /*   By: jdarcour <jdarcour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:40:50 by jdarcour          #+#    #+#             */
-/*   Updated: 2023/09/26 18:38:22 by jdarcour         ###   ########.fr       */
+/*   Updated: 2023/09/28 19:07:46 by jdarcour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
+
 void	handler(int signum)
 {
-	(void)signum;
-	write(1, "Signal received\n", 16);
+	static unsigned int		len;
+	static unsigned int		bit_count = 0;
+	static unsigned char	current_char;
+	static int				i;
+	static char				*buffer;
+
+	if (signum == SIGINT)
+	{
+		ft_printf("Received SIGINT\n");
+		if (buffer)
+			free(buffer);
+		exit(0);
+	}
+	if (bit_count < 31)
+	{
+		len <<= 1;
+		len |= (signum == SIGUSR1);
+		bit_count++;
+	}
+	else if (bit_count == 31)
+	{
+		len <<= 1;
+		len |= (signum == SIGUSR1);
+		ft_printf("Received length: %u\n", len);
+		buffer = (char *)malloc(sizeof(char) * (len + 1));
+		buffer[0] = '\0';
+		bit_count++;
+	}
+	else
+	{
+		current_char |= (signum == SIGUSR1);
+		i++;
+		if (i == 8)
+		{
+			if (current_char == '\0')
+			{
+				ft_printf("Received message: %s\n", buffer);
+				free(buffer);
+				buffer = NULL;
+				bit_count = 0;
+				len = 0;
+				current_char = 0;
+				i = 0;
+			}
+			else
+			{
+				ft_strlcat(buffer, (const char*) &current_char, len + 1);
+				i = 0;
+				current_char = 0;
+			}
+		}
+		else
+			current_char <<= 1;
+	}
 }
 
 int	main(void)
@@ -23,9 +76,12 @@ int	main(void)
 	struct sigaction	sa;
 
 	sa.sa_handler = handler;
-
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	ft_printf("Server PID: %d\n", getpid());
 	sigaction(SIGUSR1, &sa, NULL);
-	ft_printf("My pid is %d\n", getpid());
+	sigaction(SIGUSR2, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
 	while (1)
 		pause();
 	return (0);
